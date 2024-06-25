0a70a2d54f2b562e55c86631094bbf64fabb4612
Tim Langford <tim.langford@gmail.com>
2024-06-25 18:53:12 +0100

    Added basic echo builtin.

--- app/main.py
+++ app/main.py
@@ -2,17 +2,21 @@ import sys
 
 
 def handle_command(command):
-    parts = command.split(" ")
-    cmd = parts[0] if len(parts) > 0 else None
-    arg1 = parts[1] if len(parts) > 1 else None
+    cmd_tokens = command.split(" ")
+    cmd = cmd_tokens[0] if len(cmd_tokens) > 0 else None
 
     if cmd == "exit":
+        arg1 = cmd_tokens[1] if len(cmd_tokens) > 1 else None
         exit_code = 0 
         try:
             exit_code = int(arg1) if arg1 else 0
         except ValueError:
             exit_code = 0
         sys.exit(exit_code)
+    elif cmd == "echo":
+        to_echo = command[len(cmd) + 1:]
+        sys.stdout.write(to_echo)
+
     else:
         sys.stdout.write(f"{cmd}: command not found\n")
 
