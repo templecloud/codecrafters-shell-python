5ab574b7d9af25979410991dcd829071e981f908
Tim Langford <tim.langford@gmail.com>
2024-06-25 18:30:21 +0100

    Added basic REPL.

--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
 import sys
 
 
+def handle_command(command):
+    if command == "exit":
+        sys.exit(0)
+    else:
+        sys.stdout.write(f"{command}: command not found\n")
+
 def main():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    # Wait for user input
-    command = input()
-    
-    # Handle Command
-    sys.stdout.write(f"{command}: command not found\n")
+        # Wait for user input
+        command = input()
 
+        # Handle Command
+        handle_command(command)
 
 if __name__ == "__main__":
     main()
